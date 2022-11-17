ccdce76a1793b2f92edbc17e76c5b241cbdbaf00
Diego <diego@parsimotion.com>
2022-11-17 16:46:08 -0300

    Agrego schema

--- server/app.js
+++ server/app.js
@@ -5,11 +5,8 @@ Main application file
 require("./srv-globals");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
-console.log("B")
 const mongoose = require("mongoose");
-console.log("C")
 const config = require("./config/environment");
 
 mongoose.Promise = require("bluebird")
-console.log("A")
 mongoose.connect(config.mongo.uri, config.mongo.options);
---
+++ server/domain/schemas/crypto.schema.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var _ = require('lodash');
+var Schema = mongoose.Schema;
+
+CryptoSchema = new Schema({
+});
+
+
+module.exports = mongoose.model('Crypto', CryptoSchema);;
